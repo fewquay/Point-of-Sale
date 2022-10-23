f25f589e2312c506caedd6e56d8234521578ccd7
Bart Fuqua <bart.fuqua@gmail.com>
2022-10-22 20:42:59 -0500

    Update Memcached

--- POS_api.php
+++ POS_api.php
@@ -1,7 +1,9 @@
 <?php
     header("Content-Type:application/json");
-    if(class_exists('Memcache')){
-      $memcache = new Memcache;
+    $memcache = NULL;
+    if(class_exists('Memcached')){
+      $memcache = new Memcached;
+      $memcache->addServer("127.0.0.1", 11211);
     }
     $rules = array();
     $item_counts = array();
@@ -9,12 +11,12 @@
 
     function createRule($key,$price,$price_discounted,$price_discount_at_quantity,$bogo) {
       global $rules, $item_counts;
-      
+
       $rules[$key]['price'] = $price;
       $rules[$key]['price_discounted'] = $price_discounted;
       $rules[$key]['price_discount_at_quantity'] = $price_discount_at_quantity;
       $rules[$key]['bogo'] = $bogo;
-      
+
       $item_counts[$key] = 0;
     }
 
@@ -22,7 +24,7 @@
     createRule('B',10.00,0,0,'E');
     createRule('C',1.25,6.00,6,'');
     createRule('D',0.15,0,0,'');
-    createRule('E',2.00,0,0,'');  
+    createRule('E',2.00,0,0,'');
     $tax = 0.10;
 
     if (isset($_GET['function']) && $_GET['function']!="") {
@@ -36,12 +38,13 @@
             break;
         case 'init':
             init();
-            break;            
+            break;
       }
     }
 
    function init() {
       $token = uniqid();
+      global $memcache;
       if (isset($memcache)) {
         $memcache->set($token,'');
       }
@@ -50,24 +53,25 @@
     }
 
    function scan() {
+       global $memcache;
        if (isset($_GET['product']) && $_GET['product']!="") {
          $product = $_GET['product'];
        }
        else {
          exit;
        }
-     
+
       if (isset($memcache)) {
         $currentCart = $memcache->get($_GET['token']);
         $memcache->set($_GET['token'],$currentCart.$product);
-      } 
-      $response = array('product' => $product);   
+      }
+      $response = array('product' => $product);
       echo json_encode( $response );
     }
 
    function total() {
-      global $rules, $item_counts, $tax ;
-      
+      global $rules, $item_counts, $tax, $memcache;
+
       $total = 0 ;
       if (isset($_GET['cart']) && $_GET['cart']!="") {
         $cart = $_GET['cart'];
@@ -81,56 +85,54 @@
       else {
         exit;
       }
-      
+
       $products = str_split($cart);
       //zero item item counts
-      foreach ($rules as $key => $rule) {    
+      foreach ($rules as $key => $rule) {
         $item_counts[$key] = 0;
       }
       //get product counts by product name
       foreach ($products as $product) {
         $item_counts[$product]++ ;
       }
-      
+
       //first handle bogo items..remove bogo items so they will not add to price
       //  if count goes negative on any product it means the customer is not taking advantage of the bogo
       foreach ($rules as $key => $rule) {
         $bogo = $rule['bogo'] ;
-        
+
         if ( strlen($bogo) == 0 )
           continue;
-          
-        $item_counts[$bogo] -= $item_counts[$key] ;  
+
+        $item_counts[$bogo] -= $item_counts[$key] ;
       }
       //second handle package deals
       foreach ($rules as $key => $rule) {
         $price_discount_at_quantity = $rule['price_discount_at_quantity'] ;
-        
+
         if ( $price_discount_at_quantity <= 0 )
           continue;
-        
+
         $whole_packages = floor( $item_counts[$key] / $price_discount_at_quantity ) ;
-        
+
         if ( $whole_packages <= 0 )
           continue;
-          
+
         $total += $whole_packages * $rule['price_discounted'] ;
         $item_counts[$key] -= $whole_packages * $price_discount_at_quantity ;
       }
       //third count the singles that arent bogo or packages
       foreach ($rules as $key => $rule) {
         $product_count = $item_counts[$key];
-        
+
         if ( $item_counts[$key] <= 0 )
           continue;
-          
+
         $total += $item_counts[$key] * $rule['price'];
       }
-      
+
       $total *= ( 1 + $tax ) ;
 
-      $response = array('total' => $total);   
+      $response = array('total' => $total);
       echo json_encode($response);
     }
-
-
