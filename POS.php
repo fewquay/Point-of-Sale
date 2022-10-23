9af77e33d30948f72901e46543ffc2d215dc3055
Bart Fuqua <bart.fuqua@gmail.com>
2022-10-22 20:40:30 -0500

    Update POS.php

--- POS.php
+++ POS.php
@@ -4,10 +4,13 @@ require('./POS_Client.php');
 
 $terminal = new POS_Client(); echo '<br><br>';
 $cart_token = $terminal->init() ; echo '<br><br>';
-print_r( $terminal->scan("A", $cart_token) ); echo '<br><br>';
-print_r( $terminal->scan("D", $cart_token) ); echo '<br><br>';
-print_r( $terminal->total('BCDABEAAA', $cart_token));      echo '<br><br>';
-print_r( $terminal->total('CCCCCC', $cart_token));      echo '<br><br>';
-print_r( $terminal->total('ABCD', $cart_token));      echo '<br><br>';
-print_r( $terminal->total('ABECDE', $cart_token));      echo '<br><br>';
-
+print_r( $terminal->scan("B", $cart_token) ); echo '<br><br>'; 
+print_r( $terminal->scan("C", $cart_token) ); echo '<br><br>'; 
+print_r( $terminal->scan("D", $cart_token) ); echo '<br><br>'; 
+print_r( $terminal->scan("A", $cart_token) ); echo '<br><br>'; 
+print_r( $terminal->scan("B", $cart_token) ); echo '<br><br>'; 
+print_r( $terminal->scan("E", $cart_token) ); echo '<br><br>'; 
+print_r( $terminal->scan("A", $cart_token) ); echo '<br><br>'; 
+print_r( $terminal->scan("A", $cart_token) ); echo '<br><br>'; 
+print_r( $terminal->scan("A", $cart_token) ); echo '<br><br>'; 
+print_r( $terminal->total('', $cart_token)); echo '<br><br>'; 
